c84868417682572fc4c131f619e5947d434adfd5
Marwin Baumann <mbaumann@schubergphilis.com>
2025-03-25 09:52:01 +0100

    review

--- examples/project/main.tf
+++ examples/project/main.tf
@@ -30,9 +30,3 @@ module "tfe_variable_set" {
   priority          = try(each.value.global, false)
   variables         = each.value.variables
 }
-
-# Optionally, you can assign access of the variable set to a project.
-resource "tfe_project_variable_set" "test" {
-  project_id      = tfe_project.default.id
-  variable_set_id = module.tfe_variable_set["domain"].id
-}
